82baa7f6d7ead4c5820ab37a233e4ea83f1b40c4
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-29 23:28:59 +0900

    Update: js to typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NextPage } from 'next';
+import Head from 'next/head';
+import styles from 'styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className="styles.container">
+      {/* {} */}
+      hello world
+    </div>
+  )
+}
+
+export default Home;
