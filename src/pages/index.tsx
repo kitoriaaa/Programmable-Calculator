b41e549819471725b8316a9092591f532ddc28a7
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-04-04 23:55:47 +0900

    Update: home components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import styles from 'styles/Home.module.css';
+import Calc from '../../components/Calc';
 
 const Home: NextPage = () => {
   return (
-    <div className="styles.container">
-      {/* {} */}
-      hello world
-    </div>
-  )
-}
+    <>
+      <Head>
+        <title>Programmable-Calculator</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous"></link>
+      </Head>
+      <div className="styles.container">
+        <Calc />
+      </div>
+    </>
+  );
+};
 
 export default Home;
